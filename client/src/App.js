4180c76a4775c8d90ae0e4ef89d71366bcd202bc
Siddharth Rawat <siddharth.rawat94@gmail.com>
2020-05-17 18:37:58 +0530

    Apollo setup + BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import BookList from './components/BookList';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+// Apollo Client Setup
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+});
 
 function App() {
   return (
-    <div id="main">
-      <h1>Sid's Reading List</h1>
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Sid's Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
